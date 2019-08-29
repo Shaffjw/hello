54198bebcfb53285570cff8e91cfbd962942a3e7
Shaffjw <shaffjww@gmail.com>
2019-08-29 16:41:17 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*  Author: Jarrod Shaff
+    Hack #1 Hello World problem that says my name
+    Date: 29AUG2019
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello I'm Jarrod Shaff!\n");
+  printf("My major is Electrical Engineering./n");
+  return 0:
+}
